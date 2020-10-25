e6c1bbd2996bfde6370468810ad4823a66c3a4fe
JoseBurgos1993 <burgosjd1993@gmail.com>
2020-10-24 21:07:38 -0400

    Updates

---
+++ precache-manifest.940e0335569f8135acf39cb202fea861.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2298e50301bc12680e496fcfc19c3ee2",
+    "url": "/employee-directory/index.html"
+  },
+  {
+    "revision": "13efe860d0501fec83c8",
+    "url": "/employee-directory/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "72326b58ec0a80babfc4",
+    "url": "/employee-directory/static/css/main.5398f764.chunk.css"
+  },
+  {
+    "revision": "13efe860d0501fec83c8",
+    "url": "/employee-directory/static/js/2.d3e8f5db.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/employee-directory/static/js/2.d3e8f5db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72326b58ec0a80babfc4",
+    "url": "/employee-directory/static/js/main.91fc839c.chunk.js"
+  },
+  {
+    "revision": "79d7c4f4ad2a4d373d4e",
+    "url": "/employee-directory/static/js/runtime-main.759497ab.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/employeedirectory/precache-manifest.d37685a931db09011d581018f168389e.js"
+  "/employee-directory/precache-manifest.940e0335569f8135acf39cb202fea861.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/employeedirectory/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/employee-directory/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
